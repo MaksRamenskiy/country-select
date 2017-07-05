4f2e8e02b269f213f30540b51de3ea5e6e6fb587
Maxim Ramenskiy <maxim.ramenskiy@team.wrike.com>
2017-07-05 05:27:36 +0300

    add sorting

--- src/redux/reducers/userCountry.js
+++ src/redux/reducers/userCountry.js
@@ -9,7 +9,10 @@ const userCountry = (state=initialUserCountryState, action) => {
             const currentCountry = state.list.find(country => country.code === action.countryCode);
             return {...state, current: currentCountry}
         case 'COUNTRIES_LOADED':
-            return {...state, list: action.payload}
+            const sortedCountries = action.payload.sort((a,b) => {
+                return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)
+            });
+            return {...state, list: sortedCountries}
         default:
             return state;
     }
