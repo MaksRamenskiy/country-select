9496408c9391d4cd24a262d9d24143672b897df0
Maksim Ramenskiy <ramenskiy.m@gmail.com>
2020-06-30 14:59:21 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
-const sortedCountriesJsonUrl = 'https://gist.githubusercontent.com/MaksRamenskiy/78b6ea32f3e07f9c682e3c58a9a3e10f/raw/c295b2eb2ac1fb8fc882b1d3183dd5b85d75f0ef/countries.json';
-const unSortedCountriesJsonUrl = 'https://gist.githubusercontent.com/MaksRamenskiy/c565767b462f610a2f867633cfadd1e4/raw/a3c90ad992aec68c0a72d23639df0400d24c2fa2/notSortedCountries.js'
+const sortedCountriesJsonUrl = 'https://gist.githubusercontent.com/ramenski/78b6ea32f3e07f9c682e3c58a9a3e10f/raw/c295b2eb2ac1fb8fc882b1d3183dd5b85d75f0ef/countries.json';
+const unSortedCountriesJsonUrl = 'https://gist.githubusercontent.com/ramenski/c565767b462f610a2f867633cfadd1e4/raw/a3c90ad992aec68c0a72d23639df0400d24c2fa2/notSortedCountries.js'
 
 export function getCountries() {
     return fetch(sortedCountriesJsonUrl)
